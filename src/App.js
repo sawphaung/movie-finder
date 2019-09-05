8d2a57f7960d8c89eb8aa612fb2c9425ac9b03c1
Saw Phaung <saw@77creative.io>
2019-09-04 21:51:32 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.scss';
+import axios from 'axios';
 
 function App() {
+  const API_KEY = '0429b20a4ae03f613cc8a1c247b9b375';
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const movieData = async () => {
+      const result = await axios(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`
+      );
+      setMovies(result.data.results);
+      console.log(result.data.results);
+    };
+
+    movieData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='heading'>Popular Movies </h1>
+
+      <div className='App'>
+        {movies.map(movie => (
+          <div key={movie.id} className='movie'>
+            <h1 className='movie_heading'>{movie.title}</h1>
+            <div className='movie_container'>
+              <div className='movie_container_image'>
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  alt=''
+                />
+              </div>
+              <div className='movie_container_contents'>
+                <p>
+                  Overview: <span>{movie.overview}</span>
+                </p>
+                <p>Release Date: {movie.release_date}</p>
+                <p>Popularity: {movie.popularity}</p>
+                <p>Rating: {movie.vote_average} / 10</p>
+                <p>Vote Count: {movie.vote_count}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
