44ca82af1b7af609fc4626cdcef037c9ef78c9c6
Saw Phaung <saw@77creative.io>
2019-09-22 15:10:21 -0500

    carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,28 +21,30 @@ export default function Home() {
   };
 
   return (
-    <Slider {...settings}>
-      {movieSlideArray.map(heroMovie => (
-        <div key={heroMovie.title} className='hero'>
-          <div
-            style={{
-              backgroundImage: `url(https://image.tmdb.org/t/p/w1280/${heroMovie.backdrop_path}) `
-            }}
-            className='hero_bg'
-          >
-            <div className='hero_contents'>
-              <h1 className='title'>
-                {heroMovie.title}
-                <span>{heroMovie.vote_average}</span>
-              </h1>
-              <p className='overview'>{heroMovie.overview}</p>
-              <Link to={`/movies/${heroMovie.id}`} className='button'>
-                Learn More
-              </Link>
+    <div className='hero'>
+      <Slider {...settings}>
+        {movieSlideArray.map(heroMovie => (
+          <div key={heroMovie.title} className='hero_carousel'>
+            <div
+              className='hero_carousel_items'
+              style={{
+                backgroundImage: `url(https://image.tmdb.org/t/p/w1280/${heroMovie.backdrop_path}) `
+              }}
+            >
+              <div className='hero_carousel_items_contents'>
+                <h1 className='title'>
+                  {heroMovie.title}
+                  <span>{heroMovie.vote_average}</span>
+                </h1>
+                <p className='overview'>{heroMovie.overview}</p>
+                <Link to={`/movies/${heroMovie.id}`} className='button'>
+                  Learn More
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </Slider>
+        ))}
+      </Slider>
+    </div>
   );
 }
