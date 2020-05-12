1104a08f4dd46f67362701891f14710888bb57d7
Saw Phaung <saw@77creative.io>
2020-05-12 16:08:02 -0500

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,11 @@ import React from "react";
 export default function Footer() {
   return (
     <div class="footer">
+      <span>Version: 1.0</span>
       <p>
         Designed by
         <a href="https://www.sawphaung.com/">Saw Phaung</a>
       </p>
-
-      <span>Version: 1.0</span>
     </div>
   );
 }
